f30b3a908414d9004a183cbec225ab8dbabc739d
ChuukiArt <tlhb3678@gmail.com>
2026-08-18 17:31:41 +1200

    set up querty 4

---
+++ project_sql/4_top_paying_skills.sql
@@ -0,0 +1,9 @@
+/*
+Question: What are the top skills based on salary?
+- Identify the Average Salary associated with each skill for Data Analyst roles
+- Filter out job postings with no specified salary information (NULL), regardless of location.
+- Why? It reveals how different skills impact salary levels for Data Analysts, Identify the
+    most financially rewarding skill to aquire.
+*/
+
+
